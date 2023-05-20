d47136c0e09ceb783a6f5a9842cf920dc2ac4a83
Angelika Czerwińska <angelikaczerwinska9@gmail.com>
2023-05-20 12:25:32 +0200

    Added createBoard button, connected input fields with board

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
+const widthFromInput = document.querySelector(".width");
+const heightFromInput = document.querySelector(".height");
+const startStopButton = document.querySelector(".start-stop-button");
+const createBoardButton = document.querySelector(".create-board-button");
+
+createBoardButton.addEventListener("click", createBoard);
+// startStopButton.addEventListener("click", startGame);
+
 function createBoard() {
-  for (let i = 0; i < 10; i++) {
+  const widthFromInputValue = widthFromInput.value;
+  const heightFromInputValue = heightFromInput.value;
+  for (let i = 0; i < widthFromInputValue; i++) {
     const row = document.createElement("div");
     let board = document.querySelector(".game-board");
     board.appendChild(row);
     row.className = "row";
-    for (let j = 0; j < 10; j++) {
+    for (let j = 0; j < heightFromInputValue; j++) {
       const column = document.createElement("div");
       column.className = "column";
       row.appendChild(column);
     }
   }
+  // board za każdym kliknieciem się powieksza, moze klasa? Zmiana przycisku na clean board?
 }
 
-createBoard();
+// function startGame() {}
